0b5491d95013654bc524d187ca93e117564982bf
Jovial307 <pauljovial949@gmail.com>
2025-07-11 16:42:37 +0100

    finished the book of mormon app

---
+++ week02/scripts/bom.js
@@ -0,0 +1,30 @@
+const input = document.querySelector('#favchap');
+const button = document.querySelector('button');
+const list = document.querySelector('ul');
+
+
+
+button.addEventListener('click', () => {
+    if (input.value.trim() !== '') {
+        input.focus();
+        const myfav = input.value;
+        input.value = '';
+    
+        const li = document.createElement('li');
+        const listText = document.createElement('span');
+        const deleteButton = document.createElement('button');
+    
+        
+        li.appendChild(listText);
+        listText.textContent = myfav;
+        li.appendChild(deleteButton);
+        deleteButton.textContent = '❌';
+        list.appendChild(li);
+    
+        deleteButton.addEventListener('click', () => {
+            list.removeChild(li);
+        });
+    }
+
+    input.focus();
+});
